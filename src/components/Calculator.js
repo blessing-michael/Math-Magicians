6ff4a422444e0b98d899707e6831d9f5756b50b8
Blessing <blessingmike334@gmail.com>
2023-09-12 13:35:06 +0100

    Add styles for computer display

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CalculatorButtons from './calculatorButtons';
+import CalculatorDisplay from './CalculatorDisplay';
+import '../styles/style.css'
+
+const Calculator = () => (
+  <div className="calculator-container">
+  <CalculatorDisplay />
+    <CalculatorButtons />
+   
+  </div>
+);
+
+export default Calculator;
