734eb885aa98acbc849ff60dc585d7c8bb846f34
Blessing <blessingmike334@gmail.com>
2023-09-13 12:51:59 +0100

    Add calculator functionality

---
+++ src/components/CalculatorDisplay.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CalculatorDisplay = ({ displayValue }) => (
+  <>
+    <h2 className="intro-text">Your Perfect Math Calculator</h2>
+    <input type="text" value={displayValue} style={{ color: 'white' }} disabled />
+  </>
+);
+
+CalculatorDisplay.propTypes = {
+  displayValue: PropTypes.string.isRequired,
+};
+
+export default CalculatorDisplay;
